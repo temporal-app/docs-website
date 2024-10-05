19568f0e9679f13586c1f052b35802226e533a31
Andrey Patyuchenko <andrey.patyuchenko@gmail.com>
2024-10-05 18:08:21 +0200

    added docs index redirect

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,9 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	redirects: {
+		'/docs': '/docs/getting-started',
+	},
 	integrations: [
 		starlight({
 			title: 'Temporal Docs',
